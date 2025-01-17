0e4a6a8acfbb2b239a273852889782f05944eab5
langhuihui <178529795@qq.com>
2025-01-17 09:06:37 +0800

    优化 ts 导出

--- packages/decoder/src/index.ts
+++ packages/decoder/src/index.ts
@@ -1,6 +1,6 @@
-export { VideoDecoderSoft } from "./video_decoder_soft";
-export { VideoDecoderSoftSIMD } from "./video_decoder_soft_simd";
-export { VideoDecoderHard } from "./video_decoder_hard";
-export { VideoDecoderMSE } from "./video_decoder_mse";
-export { AudioDecoderSoft } from "./audio_decoder_soft";
-export { AudioDecoderHard } from "./audio_decoder_hard";
+export * from "./video_decoder_soft";
+export * from "./video_decoder_soft_simd";
+export * from "./video_decoder_hard";
+export * from "./video_decoder_mse";
+export * from "./audio_decoder_soft";
+export * from "./audio_decoder_hard";
--- packages/decoder/src/video_decoder_soft.ts
+++ packages/decoder/src/video_decoder_soft.ts
@@ -1,7 +1,7 @@
 import { VideoDecoderSoftBase } from './video_decoder_soft_base';
 import CreateModule from '../wasm/types/videodec';
 export class VideoDecoderSoft extends VideoDecoderSoftBase {
-  constructor(opt?: { workerMode: boolean, yuvMode:boolean, canvas: HTMLCanvasElement, wasmPath: string; }) {
-    super(CreateModule, opt?.wasmPath?fetch(opt?.wasmPath).then(res => res.arrayBuffer()):void 0, opt?.workerMode, opt?.canvas, opt?.yuvMode);
+  constructor(opt?: { workerMode?: boolean, yuvMode?: boolean, canvas?: HTMLCanvasElement, wasmPath?: string; }) {
+    super(CreateModule, opt?.wasmPath ? fetch(opt?.wasmPath).then(res => res.arrayBuffer()) : void 0, opt?.workerMode, opt?.canvas, opt?.yuvMode);
   };
 };
